fb4e41dd8ad9321f2bbe7daf2b2bdc80db5af398
Stas Boukarev <stassats@gmail.com>
2008-04-28 22:50:01 +0400

    Adjust font.

--- init_iface.el
+++ init_iface.el
@@ -8,12 +8,11 @@
 
 (if window-system
     (progn
-      (set-default-font "-misc-fixed-medium-r-normal--18-120-100-100-c-90-iso10646-1")
       (set-face-font 'menu "-misc-fixed-medium-r-normal--18-120-100-100-c-90-iso10646-1")
       (add-to-list 'default-frame-alist
-		   '(font . "-misc-fixed-medium-r-normal--18-120-100-100-c-90-iso10646-1"))
+		   '(font . "-misc-fixed-medium-r-normal--18-120-*-*-*-*-iso8859-1"))
       (global-set-key "\C-x\C-y" 'x-clipboard-yank))
-  (global-set-key "\C-h" 'backward-delete-char))
+    (global-set-key "\C-h" 'backward-delete-char))
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
