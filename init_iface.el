0224ce8e3ce35865b7138f96107de5954133f375
Stas Boukarev <stassats@gmail.com>
2009-03-13 22:05:49 +0300

    Fix fonts.

--- init_iface.el
+++ init_iface.el
@@ -5,13 +5,11 @@
   (color-theme-initialize)
   (color-theme-charcoal-black))
 
-(set-fontset-font "fontset-default" 'cyrillic '("arial" . "unicode-bmp"))
-
-(let ((font "-misc-fixed-medium-r-normal--18-120-100-100-c-90-iso10646-1"))
-  (add-to-list 'default-frame-alist `(font . ,font)))
-
 (if window-system
-    (global-set-key "\C-x\C-y" 'x-clipboard-yank)
+    (let ((font "-misc-fixed-medium-r-normal--18-120-100-100-c-90-iso10646-1"))
+      (set-fontset-font "fontset-default" 'cyrillic '("arial" . "unicode-bmp"))
+      (add-to-list 'default-frame-alist `(font . ,font))
+      (global-set-key "\C-x\C-y" 'x-clipboard-yank))
     (global-set-key "\C-h" 'backward-delete-char))
 
 (tool-bar-mode -1)
