ff53dd674ba337eecc448d06e418d271d3942c97
Stas Boukarev <stassats@gmail.com>
2010-07-25 00:31:32 +0400

    Some changes regarding paredit.

--- init_lisp.el
+++ init_lisp.el
@@ -9,10 +9,59 @@
 (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
 
+(put :default-initargs
+     'common-lisp-indent-function '(&rest))
+
 (require-and-eval (paredit paredit)
-  ;; Load patches
-  ;;(mapc 'load (directory-files "~/.emacs.d/paredit/" t "paredit-.+\\.el"))
-  )
+
+  (defvar paredit-space-for-delimiter-predicates nil)
+
+  (defun paredit-space-for-delimiter-p (endp delimiter)
+    ;; If at the buffer limit, don't insert a space.  If there is a word,
+    ;; symbol, other quote, or non-matching parenthesis delimiter (i.e. a
+    ;; close when want an open the string or an open when we want to
+    ;; close the string), do insert a space.
+    (and (not (if endp (eobp) (bobp)))
+         (memq (char-syntax (if endp (char-after) (char-before)))
+               (list ?w ?_ ?\"
+                     (let ((matching (matching-paren delimiter)))
+                       (and matching (char-syntax matching)))
+                     (and (not endp)
+                          (eq ?\" (char-syntax delimiter))
+                          ?\) )))
+         (catch 'exit
+           (dolist (predicate paredit-space-for-delimiter-predicates)
+             (if (not (funcall predicate endp delimiter))
+                 (throw 'exit nil)))
+           t)))
+
+  (defvar common-lisp-octothorpe-quotation-characters '(?P))
+  (defvar common-lisp-octothorpe-parameter-parenthesis-characters '(?A))
+  (defvar common-lisp-octothorpe-parenthesis-characters '(?+ ?- ?C))
+
+  (defun paredit-space-for-delimiter-predicate-common-lisp (endp delimiter)
+    (or endp
+        (let ((case-fold-search t)
+              (look
+               (lambda (prefix characters n)
+                 (looking-back
+                  (concat prefix (regexp-opt (mapcar 'string characters)))
+                  (- (point) n)))))
+          (let ((oq common-lisp-octothorpe-quotation-characters)
+                (op common-lisp-octothorpe-parenthesis-characters)
+                (opp common-lisp-octothorpe-parameter-parenthesis-characters))
+            (cond ((eq (char-syntax delimiter) ?\()
+                   (and (not (funcall look "#" op 2))
+                        (not (funcall look "#[0-9]*" opp 20))))
+                  ((eq (char-syntax delimiter) ?\")
+                   (not (funcall look "#" oq 2)))
+                  (else t))))))
+
+  (add-hook 'lisp-mode-hook
+            (defun common-lisp-mode-hook-paredit ()
+              (make-local-variable 'paredit-space-for-delimiter-predicates)
+              (add-to-list 'paredit-space-for-delimiter-predicates
+                           'paredit-space-for-delimiter-predicate-common-lisp))))
 
 ;; (require-and-eval (redshank redshank))
 
@@ -28,10 +77,11 @@
 
 (require-and-eval (slime slime)
   (defun load-slime ()
-    (slime-setup '(slime-fancy slime-sbcl-exts slime-scheme
-                   slime-sprof ;; slime-gauche
-                   )) ;; slime-asdf
-    
+    (slime-setup '(slime-fancy
+                   slime-sbcl-exts slime-scheme
+                   slime-sprof slime-asdf
+                   )) ;; slime-gauche
+    (define-key slime-mode-map "\C-i" 'slime-indent-and-complete-symbol)
     (setq
      lisp-indent-function 'common-lisp-indent-function
      slime-complete-symbol-function 'slime-fuzzy-complete-symbol
@@ -47,17 +97,11 @@
                                   "/home/stas/lisp/fasls/from-slime/")
      slime-repl-history-remove-duplicates t
      slime-repl-history-trim-whitespaces t
-     slime-fuzzy-explanation "")
+     slime-fuzzy-explanation ""
+     slime-repl-history-file "~/.config/emacs/slime-history.eld")
     
     (define-key slime-repl-mode-map "\C-c\C-u" 'slime-repl-delete-current-input)
-    (define-key slime-mode-map "\C-c\M-d"
-      (lambda ()
-        (interactive)
-        (let ((form (slime-parse-toplevel-form)))
-          (when (and (eql (car form) :defun)
-                     (cadr form))
-            (slime-disassemble-symbol
-             (symbol-name (cadr form)))))))
+    (define-key slime-editing-map "\C-c\M-d" 'slime-disassemble-definition)
     
     (substitute-key-definition 'slime-xref-next-line 'next-line
                                slime-xref-mode-map)
@@ -95,7 +139,7 @@
                                           (slime ,path ',coding))))))
 
     (define-lisps
-      (abcl nil iso-8859-1-unix)
+        (abcl nil iso-8859-1-unix)
         cmucl
       ccl clisp scl acl ecl
       (lw nil iso-8859-1-unix)))
@@ -116,9 +160,9 @@
 
 (require 'quack nil t)
 
-(require-and-eval (lisppaste)
-  (push '("http://paste\\.lisp\\.org/\\(\\+\\)\\|\\(display\\)" . lisppaste-browse-url)
-        browse-url-browser-function))
+;; (require-and-eval (lisppaste)
+;;   (push '("http://paste\\.lisp\\.org/\\(\\+\\)\\|\\(display\\)" . lisppaste-browse-url)
+;;         browse-url-browser-function))
 
 (add-hook 'scheme-mode-hook
           (lambda ()
@@ -193,6 +237,7 @@
   (define-key mode "\M-," 'jump-back))
 
 (define-key emacs-lisp-mode-map "\C-c\C-z" 'jump-to-ielm-buffer)
+(define-key ielm-map "\C-j" 'newline-and-indent)
 
 (defun set-neighbour-buffer ()
   (set-buffer (window-buffer (next-window))))
