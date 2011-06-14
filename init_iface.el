0caa8812350ab43d809a6e177d7ab7d420ba0460
Stas Boukarev <stassats@gmail.com>
2011-06-15 00:33:51 +0400

    Adjust fonts for laptop.

--- init_iface.el
+++ init_iface.el
@@ -1,12 +1,19 @@
 
 (setq inhibit-startup-message t
-      font-lock-maximum-decoration nil)
+      font-lock-maximum-decoration '((lisp-mode . nil)
+                                     (emacs-lisp-mode . nil)
+                                     (scheme-mode . nil)
+                                     (t . t)))
+(defvar *default-font*
+  (if (equal system-name "laptop")
+      "DejaVu Sans Mono 14"
+      "DejaVu Sans Mono 13"))
 
 (when window-system
   (global-set-key "\C-x\C-y" 'x-clipboard-yank)
   (setq default-frame-alist
         (acons 'font
-               "DejaVu Sans Mono 13"
+               *default-font*
                default-frame-alist))
   (tool-bar-mode -1)
   (scroll-bar-mode -1))
