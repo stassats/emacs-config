9f88c14c930933ce557d2aef2992786db144fb51
Stas Boukarev <stassats@gmail.com>
2011-10-14 00:29:34 +0400

    config_share.el: style changes.

--- config_share.el
+++ config_share.el
@@ -3,17 +3,18 @@
 (defun add-to-path (dir)
   "Add directory `dir' in ~/.emacs.d/ to `load-path'."
   (let ((name
-         (typecase dir
-           (symbol (format "~/.emacs.d/%s" dir))
-           (string dir))))
-    (if (file-exists-p name)
-        (add-to-list 'load-path name))))
+          (etypecase dir
+            (symbol (format "~/.emacs.d/%s" dir))
+            (string dir))))
+    (when (file-exists-p name)
+      (add-to-list 'load-path name))))
 
 (defmacro* require-and-eval ((feature &optional add-to-path) &body body)
   "Execute code if feature was loaded successfully.
 Optinally add directory `add-to-path' to `load-path'."
   `(progn
-     ,(if add-to-path `(add-to-path ',add-to-path))
+     ,(when add-to-path
+        `(add-to-path ',add-to-path))
      (when (require ',feature nil t)
        ,@body)))
 
