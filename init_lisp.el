34dfb0e5f36a82af9e1cb3f31bb7a47919c748b8
Stas Boukarev <stassats@gmail.com>
2012-03-06 05:48:50 +0400

    Set slime-export-save-file to t.

--- init_lisp.el
+++ init_lisp.el
@@ -115,7 +115,8 @@
      lisp-loop-indent-subclauses nil
      lisp-loop-indent-forms-like-keywords t
      lisp-lambda-list-keyword-parameter-alignment t
-     slime-export-symbol-representation-auto t)
+     slime-export-symbol-representation-auto t
+     slime-export-save-file t)
     
     (define-key slime-repl-mode-map "\C-c\C-u" 'slime-repl-delete-current-input)
     (define-key slime-mode-map "\C-c\M-i" 'slime-inspect-definition)
