94a9f2b448b7ee0288de63f35c80cf81609e9ab9
Stas Boukarev <stassats@gmail.com>
2012-04-14 03:28:51 +0400

    More w3m customization.

--- init_web.el
+++ init_web.el
@@ -2,13 +2,15 @@
 
 (require-and-eval (w3m w3m)
   (require 'w3m-load)
+  (make-directory "/tmp/w3m/" t)
 
   (setq
    w3m-use-cookies t
    w3m-default-display-inline-images t
    w3m-use-favicon t
    w3m-message-silent t
-   w3m-default-save-directory "/tmp/"
+   w3m-default-save-directory "/tmp/w3m/"
+   w3m-profile-directory "/tmp/w3m/"
    w3m-keep-arrived-urls 0
    w3m-resize-images nil) ; because it's slow
 
